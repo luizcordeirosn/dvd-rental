eeee22b0ee46862f4f1130bbda2081d319ffb012
Luiz Neto <luizcsneto@outlook.com>
2023-05-09 23:17:25 -0300

    Remover as nativeQuery de idioma e implementar o HQL no modelo

--- dvdrental/src/main/java/com/project/dvdrental/Filme/Repository/IdiomaJpaRepository.java
+++ dvdrental/src/main/java/com/project/dvdrental/Filme/Repository/IdiomaJpaRepository.java
@@ -9,8 +9,8 @@ import com.project.dvdrental.Filme.Model.Idioma;
 @Repository
 public interface IdiomaJpaRepository extends JpaRepository<Idioma, Integer> {
 
-    @Query(value = "select l.* from language l"
-            + " inner join film f on f.language_id = l.language_id"
-            + " where f.film_id = :filmid", nativeQuery = true)
-    Idioma getByFilm(@Param("filmid") Integer filmeId);
+    // @Query(value = "select l.* from language l"
+    //         + " inner join film f on f.language_id = l.language_id"
+    //         + " where f.film_id = :filmid", nativeQuery = true)
+    // Idioma getByFilm(@Param("filmid") Integer filmeId);
 }
--- dvdrental/src/main/java/com/project/dvdrental/Filme/Service/IdiomaServiceImpl.java
+++ dvdrental/src/main/java/com/project/dvdrental/Filme/Service/IdiomaServiceImpl.java
@@ -1,6 +1,9 @@
 package com.project.dvdrental.Filme.Service;
 
 import java.util.Optional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.project.dvdrental.Filme.Model.Idioma;
@@ -8,18 +11,21 @@ import com.project.dvdrental.Filme.Repository.IdiomaJpaRepository;
 
 @Service
 public class IdiomaServiceImpl implements IdiomaService {
-    
+
     @Autowired
     private IdiomaJpaRepository idiomaJpaRepo;
 
+    @Autowired
+    private SessionFactory sf;
+
     @Override
     public Idioma obterPorIdIdioma(Integer id) {
-        
+
         Optional<Idioma> optionalIdioma = idiomaJpaRepo.findById(id);
 
         Idioma idioma = null;
 
-        if(optionalIdioma.isPresent()){
+        if (optionalIdioma.isPresent()) {
             idioma = optionalIdioma.get();
         }
 
@@ -28,8 +34,16 @@ public class IdiomaServiceImpl implements IdiomaService {
 
     @Override
     public Idioma obterPorFilmeIdioma(Integer filme) {
-        
-        return idiomaJpaRepo.getByFilm(filme);
+
+        Session session = sf.openSession();
+
+        String HQL = "select i from Idioma i"
+                + " inner join Filme f on f.idioma = i.id"
+                + " where f.filmeId = " + filme;
+
+        Idioma idioma = session.createQuery(HQL, Idioma.class).uniqueResult();
+
+        return idioma;
     }
-    
+
 }
