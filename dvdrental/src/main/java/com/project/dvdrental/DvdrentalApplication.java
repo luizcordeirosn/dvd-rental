ffb433780add9bbdc857dadbed0bd374dcc2ea25
Luiz Neto <luizcsneto@outlook.com>
2023-08-02 10:33:39 -0300

    Criação do Dockerfile e do docker-compose do projeto

--- dvdrental/src/main/java/com/project/dvdrental/DvdrentalApplication.java
+++ dvdrental/src/main/java/com/project/dvdrental/DvdrentalApplication.java
@@ -2,8 +2,10 @@ package com.project.dvdrental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:application.properties")
 public class DvdrentalApplication {
 
 	public static void main(String[] args) {
